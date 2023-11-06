840ac2d5bdf2eaf1002e7275ba3aea1fa2b7b3b0
Utpal Kumar Jha <utpalkumarjha@hotmail.com>
2023-11-06 17:26:41 +0530

    Frontend code
    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [jobDescriptionPrompt, setJobDescriptionPrompt] = useState('');
+    const [questionsPrompt, setQuestionsPrompt] = useState('');
+    const [jobDescription, setJobDescription] = useState('');
+    const [questions, setQuestions] = useState('');
+
+    const handleJobDescriptionChange = (e) => {
+        setJobDescriptionPrompt(e.target.value);
+    };
+
+    const handleQuestionsPromptChange = (e) => {
+        setQuestionsPrompt(e.target.value);
+    };
+
+    const handleGenerateJobDescription = async () => {
+        try {
+            const response = await axios.post('http://localhost:3000/api/generate/jobDescriptionPrompt', { prompt: jobDescriptionPrompt });
+            setJobDescription(response.data.generatedText);
+            setQuestionsPrompt(`Based on the above job description, generate 5 multiple choice questions for potential candidates:`);
+        } catch (error) {
+            console.error('Error generating job description:', error);
+        }
+    };
+
+    const handleGenerateQuestions = async () => {
+        try {
+            const response = await axios.post('http://localhost:3000/api/generate/questionPrompt', { prompt: questionsPrompt });
+            setQuestions(response.data.generatedText);
+        } catch (error) {
+            console.error('Error generating questions:', error);
+        }
+    };
+
+    return (
+        <div>
+            <h1>Job Description and Questions Generator</h1>
+            <div>
+                <textarea
+                    value={jobDescriptionPrompt}
+                    onChange={handleJobDescriptionChange}
+                    placeholder="Enter job description prompt"
+                />
+                <button onClick={handleGenerateJobDescription}>Generate Job Description</button>
+            </div>
+            {jobDescription && (
+                <div>
+                    <h2>Job Description</h2>
+                    <pre>{jobDescription}</pre> {/* Using pre tag to preserve whitespace and line breaks */}
+                    <textarea
+                        value={questionsPrompt}
+                        onChange={handleQuestionsPromptChange}
+                    />
+                    <button onClick={handleGenerateQuestions}>Generate Questions</button>
+                </div>
+            )}
+            {questions && (
+                <div>
+                    <h2>Questions</h2>
+                    <pre>{questions}</pre> {/* Using pre tag to preserve whitespace and line breaks */}
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default App;
